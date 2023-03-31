342200bd8e8e435d895739a8cf03e232b85fb313
SAZZAD HOSSAIN TASNIM <sazzadaiub1@gmail.com>
2023-04-01 00:13:46 +0600

    Add project files.

---
+++ TR I FO   Inventry VERSION -2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TR_I_FO___Inventry_VERSION__2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
